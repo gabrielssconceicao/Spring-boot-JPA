aef2292c41c79ee1d14b8f5a08c69d4731e903d8
Gabriel Conceição <gabrielc2019@outlook.com.br>
2024-06-17 09:50:52 -0300

    Product

--- src/main/java/com/educandoweb/springBootJPA/config/TestConfig.java
+++ src/main/java/com/educandoweb/springBootJPA/config/TestConfig.java
@@ -2,11 +2,14 @@ package com.educandoweb.springBootJPA.config;
 
 import com.educandoweb.springBootJPA.entities.Category;
 import com.educandoweb.springBootJPA.entities.Order;
+import com.educandoweb.springBootJPA.entities.Product;
 import com.educandoweb.springBootJPA.entities.User;
 import com.educandoweb.springBootJPA.entities.enums.OrderStatus;
 import com.educandoweb.springBootJPA.repositories.CategoryRepository;
 import com.educandoweb.springBootJPA.repositories.OrderRepository;
+import com.educandoweb.springBootJPA.repositories.ProductRepository;
 import com.educandoweb.springBootJPA.repositories.UserRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -28,6 +31,9 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Autowired
+    private ProductRepository productRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
@@ -38,14 +44,21 @@ public class TestConfig implements CommandLineRunner {
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
+        userRepository.saveAll(Arrays.asList(u1,u2));
+        orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+
         Category cat1 = new Category(null, "Electronics");
         Category cat2 = new Category(null, "Books");
         Category cat3 = new Category(null, "Computers");
 
+        Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+        Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+        Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+        Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+        Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 
-        userRepository.saveAll(Arrays.asList(u1,u2));
-        orderRepository.saveAll(Arrays.asList(o1,o2,o3));
         categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+        productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 
     }
 }
--- src/main/java/com/educandoweb/springBootJPA/entities/Category.java
+++ src/main/java/com/educandoweb/springBootJPA/entities/Category.java
@@ -5,12 +5,12 @@ import jakarta.persistence.Table;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-
-
-
+import jakarta.persistence.Transient;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_category")
@@ -21,6 +21,9 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @Transient
+    private Set<Product> products = new HashSet<>();
 
     public  Category() {}
 
@@ -43,6 +46,10 @@ public class Category implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
     }
 
     @Override
