ff6b3f160762fe7e87d6e30128d4ed790a932614
Gabriel Conceição <gabrielc2019@outlook.com.br>
2024-06-18 10:01:20 -0300

    Exceeption handling - findById

--- src/main/java/com/educandoweb/springBootJPA/services/UserService.java
+++ src/main/java/com/educandoweb/springBootJPA/services/UserService.java
@@ -2,6 +2,7 @@ package com.educandoweb.springBootJPA.services;
 
 import com.educandoweb.springBootJPA.entities.User;
 import com.educandoweb.springBootJPA.repositories.UserRepository;
+import com.educandoweb.springBootJPA.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public  User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(()-> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
