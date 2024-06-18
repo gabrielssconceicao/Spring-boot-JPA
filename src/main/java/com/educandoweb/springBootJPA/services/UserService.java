616eb8b622971d5370cc899d1b77bb6cb477599c
Gabriel Conceição <gabrielc2019@outlook.com.br>
2024-06-18 10:31:47 -0300

    Exception handler - update

--- src/main/java/com/educandoweb/springBootJPA/services/UserService.java
+++ src/main/java/com/educandoweb/springBootJPA/services/UserService.java
@@ -4,6 +4,7 @@ import com.educandoweb.springBootJPA.entities.User;
 import com.educandoweb.springBootJPA.repositories.UserRepository;
 import com.educandoweb.springBootJPA.services.exceptions.DataBaseException;
 import com.educandoweb.springBootJPA.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -42,9 +43,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity,obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
